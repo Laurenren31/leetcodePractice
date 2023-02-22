f8b4de22af74bb45a7ed9be8127aa1a0518a26d4
“Lauren” <maggie40796@gmail.com>
2023-02-23 05:42:18 +0800

    230223 not finish

--- 876 [Easy] Middle of the Linked List.js
+++ 876 [Easy] Middle of the Linked List.js
@@ -1,11 +1,11 @@
 // Input: head = [1,2,3,4,5]
 // Output: [3,4,5]
 
-const middleNode = (arr) => {
-  if (arr.length % 2 == 0) {
-    return arr.slice(arr.length / 2, arr.length);
-  }
-  if (arr.length % 2 == 1) {
-    return arr.slice(arr.length / 2 + 1, arr.length);
-  }
-};
+// const middleNode = (arr) => {
+//   if (arr.length % 2 == 0) {
+//     return arr.slice(arr.length / 2, arr.length);
+//   }
+//   if (arr.length % 2 == 1) {
+//     return arr.slice(arr.length / 2 + 1, arr.length);
+//   }
+// };
